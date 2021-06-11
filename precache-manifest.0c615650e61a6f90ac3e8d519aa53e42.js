f1accb9a9ff6ebb79cbb5a703d41cf431c0d8184
Lnx <46533182+lnxAv@users.noreply.github.com>
2021-06-10 22:35:47 -0400

    Updates

---
+++ precache-manifest.0c615650e61a6f90ac3e8d519aa53e42.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26640f08551836e643f59aefc61fc377",
+    "url": "/OpenBrewery-Map/index.html"
+  },
+  {
+    "revision": "1d70689934c75d469dd4",
+    "url": "/OpenBrewery-Map/static/css/2.e69b65fe.chunk.css"
+  },
+  {
+    "revision": "1d70689934c75d469dd4",
+    "url": "/OpenBrewery-Map/static/js/2.d5eb5d1c.chunk.js"
+  },
+  {
+    "revision": "36dbe1d3fad1bc5c8e325e31de2a1da6",
+    "url": "/OpenBrewery-Map/static/js/2.d5eb5d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb14e6d3650178f5660",
+    "url": "/OpenBrewery-Map/static/js/main.86f5f0ca.chunk.js"
+  },
+  {
+    "revision": "94af27569743854310794585e62e906a",
+    "url": "/OpenBrewery-Map/static/js/main.86f5f0ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d31f0eb0cc22ddff2a9",
+    "url": "/OpenBrewery-Map/static/js/runtime-main.6108446e.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/OpenBrewery-Map/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/OpenBrewery-Map/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/OpenBrewery-Map/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/OpenBrewery-Map/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/OpenBrewery-Map/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/OpenBrewery-Map/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/OpenBrewery-Map/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/OpenBrewery-Map/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/OpenBrewery-Map/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/OpenBrewery-Map/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/OpenBrewery-Map/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/OpenBrewery-Map/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/OpenBrewery-Map/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/OpenBrewery-Map/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/OpenBrewery-Map/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/OpenBrewery-Map/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
